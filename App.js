ae91dcaa452dcf6af3927f3fca2c012152f7f0d1
maximilian clarke <maximilian-jc@live.com>
2021-04-20 16:04:57 +0100

    button works but is buggy, doesnt redirect anywhere

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { FlatList, KeyboardAvoidingView, Image, StyleSheet, Text, View } from 'react-native';
+import { FlatList, KeyboardAvoidingView, Image, StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MarbleInput from './components/MarbleInput'
-import Marble from './components/Marble'
+import MarbleInput from './components/MarbleInput';
+import Marble from './components/Marble';
 import Login from './components/login';
 import Signup from './components/signup';
 // import Dashboard from './components/dashboard'
 import Profile from './components/profile';
-import HomeScreen from './components/homeScreen'
+import HomeScreen from './components/homeScreen';
+import firebase from './database/firebase';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
+
+  var loggedIn;
+  firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+      loggedIn = true
+    } else {
+      loggedIn = false
+    }
+  })
+
   return (
+
     <Stack.Navigator
       initialRouteName="Login"
       screenOptions={{
-
         headerTitle: 'Marble',
+        headerRight: () => (
+          loggedIn ?
+            <Button
+                onPress={() => navigation.navigate('Profile')}
+              title="Info"
+              color="#fff"
+            /> : null),
         headerTitleAlign: 'center',
         headerStyle: {
           backgroundColor: '#FAF5F0',
@@ -34,30 +52,30 @@ function MyStack() {
       <Stack.Screen
         name="Signup"
         component={Signup}
-        options={{ title: 'Signup' }}
+        options={{title: 'Signup'}}
       />
       <Stack.Screen
         name="Login"
         component={Login}
         options={
           {title: 'Login'},
-          {headerLeft: null}
+        {headerLeft: null}
         }
       />
       <Stack.Screen
-       name="HomeScreen"
-       component={HomeScreen}
-       options={
-         { title: 'Dashboard' }      
+        name="HomeScreen"
+        component={HomeScreen}
+        options={
+          {title: 'Dashboard'}
         }
       />
       <Stack.Screen
-       name="Profile"
-       component={Profile}
-       options={
-         { title: 'Profile' },
-         {headerLeft: null}
-       }
+        name="Profile"
+        component={Profile}
+        options={
+          {title: 'Profile'},
+        {headerLeft: null}
+        }
       />
     </Stack.Navigator>
   );
@@ -66,7 +84,7 @@ function MyStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <MyStack />
+      <MyStack/>
     </NavigationContainer>
   );
 }
